6494fc3cfd1bfaf58d16fb9d5b839994ebb137a0
der-blaue-elefant <github@kklein.de>
2017-08-18 11:05:22 +0200

    bla

--- generate_gnuplot_and_datafile.py
+++ generate_gnuplot_and_datafile.py
@@ -1,27 +1,39 @@
 #!/usr/bin/python3
+
 import csv
 import glob
 from datetime import datetime
 import dateutil.relativedelta
 from statistics import mean
+from collections import namedtuple
+RT = namedtuple('RT', ['rain', 'temp'])
 
 one_month = dateutil.relativedelta.relativedelta(months=1)
 for datafilename in glob.glob('*_data.csv'):
 	metafilename = datafilename.replace('_data.csv', '_meta.txt')
 	with open(datafilename, 'r') as datafile:
 		datareader = csv.reader(datafile, delimiter=';')
-		next(datareader, None) # skip header line
-		# nice to have future feature: detect correct column number via
-		# header line instead of hardcoding the columns below
+		header_row = next(datareader)
+		for n,col in enumerate(header_row):
+			if col=='MESS_DATUM_BEGINN':
+				COL_MONTH = n
+			elif col=='MO_TT':
+				COL_TEMP = n
+			elif col=='MO_RR':
+				COL_RAIN = n
 		data = {}
 		for row in datareader:
-			month = datetime.strptime(row[1], '%Y%m%d')
-			rainfall = row[5]
-			if rainfall == -999: #error value
+			month = datetime.strptime(row[COL_MONTH], '%Y%m%d')
+			rain = row[COL_RAIN]
+			temp = row[COL_TEMP]
+			if rain == '-999' or temp == '-999': #error value
 				continue
-			data[month] = rainfall
+			data[month] = RT(rain,temp)
 		# find youngest 30 complete years
-		end = max([d for d in data])
+		try:
+			end = max([d for d in data])
+		except ValueError:
+			continue
 		for i in range(0,30*12):
 			start = end - i*one_month
 			if not start in data:
@@ -34,5 +46,11 @@ for datafilename in glob.glob('*_data.csv'):
 				monthly_data = []
 				for d in data:
 					if int(d.strftime('%-m'))==i:
-						monthly_data.append(float(data[d]))
-				monthly_avg.append(mean(monthly_data))
+						monthly_data.append((float(data[d][0]),\
+							float(data[d][1])))
+				monthly_avg.append(RT(mean([m[0] for m in monthly_data]),\
+					mean([m[1] for m in monthly_data])))
+			with open(datafilename.replace('_data.csv', '_data2.csv'),'w') as outfile:
+				writer = csv.writer(outfile, delimiter=';')
+				for n,avg in enumerate(monthly_avg):
+					writer.writerow([n+1, round(avg.rain,2), round(avg.temp,2)])
